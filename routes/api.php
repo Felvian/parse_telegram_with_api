32d7737e1149cc56621c9bdb37fa0fff3e7ff4dd
Панкратова Анастасия < @ana>
2025-07-14 17:56:09 +0700

    qwe

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/add_post', [\App\Http\Controllers\PostsController::class, 'store']);
+Route::post('/add_post', [\App\Http\Controllers\Api\EventController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
