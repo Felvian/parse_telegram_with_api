70071ab6a6fb3d91494bc38bfbd15868cf9a6776
Панкратова Анастасия < @ana>
2025-07-16 09:03:44 +0700

    env + migration

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -22,8 +22,7 @@ class EventController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+
      */
     public function store(Request $request): JsonResponse
     {
